896b6432345335acc556c23944f7f3e6bfb91458
alifakhoury-git-at-934691756949 <alifakhoury.af@gmail.com>
2021-07-14 15:45:32 -0230

    Updated config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_u1laL5yEn', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7u7a3q1v3lqrv434js2ki2c0le', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'us-east-1_jTBO5rkRP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6cdf7rro7g9nan2pvt506qltur', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://tfyxh265h2.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
