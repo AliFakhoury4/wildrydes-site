0e0df62e568ecb59a4fa46745c64a88e23c1bb3e
alifakhoury-git-at-934691756949 <alifakhoury.af@gmail.com>
2021-07-14 02:17:02 -0230

    New v3

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_u1laL5yEn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7u7a3q1v3lqrv434js2ki2c0le', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://tfyxh265h2.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
